1c3a314e424fe111fe6edf1de81952e7af120f66
Ahmad Javed <ahmadjaved97@gmail.com>
2025-02-24 00:40:57 +0100

    Add HVU support in text_prompt

--- src/prompt.py
+++ src/prompt.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 from collections import OrderedDict
 import json
+import pandas as pd
 
 
 def convert_to_token(xh):
@@ -12,7 +13,7 @@ def convert_to_token(xh):
 
 def text_prompt(dataset='HMDB51', clipbackbone='ViT-B/16', device='cpu'):
     actionlist, actionprompt, actiontoken = [], {}, []
-    numC = {'HMDB51-feature-30fps-center': 51,}
+    numC = {'HMDB51-feature-30fps-center': 51, 'HVU': 66}
 
     # load the CLIP model
     clipmodel, _ = clip.load(clipbackbone, device=device, jit=False)
@@ -26,6 +27,11 @@ def text_prompt(dataset='HMDB51', clipbackbone='ViT-B/16', device='cpu'):
         meta.close()
         actionlist = np.array([a.decode('utf-8').split('\n')[0] for a in actionlist])
         actiontoken = np.array([convert_to_token(a) for a in actionlist])
+    elif dataset == 'HVU':
+        label_df = pd.read_csv("/ssd4/ajaved/evaluation_repos/open_vocab/label_files/open_vocab_all_categories_labels.csv")  # Assuming the label file is in this location
+        actionlist = label_df['label'].tolist()  # Extract action labels
+        numC['HVU'] = len(actionlist)  # Set dynamic class count for HVU
+        actiontoken = np.array([convert_to_token(a) for a in actionlist])  # Tokenize action labels
     # More datasets to be continued
 
     # query the vector from dictionary
